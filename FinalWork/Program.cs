1647b593930c2f357f841b7a1119fce06fd528dc
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-07-30 16:25:03 +0300

    FinalControlWork

---
+++ FinalWork/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.WriteLine("Введите слова через пробел:");
+string input = Console.ReadLine();
+
+string[] originalArray = input.Split(' ');
+
+// Подсчитываем количество элементов в новом массиве
+int newArrayLength = 2;
+foreach (string word in originalArray)
+{
+    if (word.Length <= 2) newArrayLength++;
+}
+
+// Создаем новый массив с заданным размером
+string[] newArray = new string[newArrayLength];
+
+// Заполняем новый массив
+int index = 0;
+foreach (string word in originalArray)
+{
+    if (index <= 2)
+    {
+        newArray[index] = word;
+        index++;
+    }
+}
+
+// Выводим новый массив
+Console.WriteLine("Новый массив:");
+foreach (string word in newArray)
+{
+    Console.Write($"{word} ");
+}
+
+
